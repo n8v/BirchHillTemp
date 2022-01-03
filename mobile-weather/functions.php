7c975376aeb30360bebcb3a6d9a77baab86ec8ea
Nathan Vonnahme <nathan@careevolution.com>
2022-01-03 10:03:32 -0900

    workaround wrong future dates in wx data

--- mobile-weather/functions.php
+++ mobile-weather/functions.php
@@ -8,7 +8,7 @@ function serveFromCache ($cachefile) {
   if (file_exists($cachefile)) {
     $last_modified_time = filemtime($cachefile);
 
-    if ($now[0] < $last_modified_time + $CACHE_TTL_SECONDS) {
+    if ( $now[0] < $last_modified_time + $CACHE_TTL_SECONDS && $last_modified_time <= $now[0] ) {
       $etag = md5_file($cachefile);
       header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified_time)." GMT");
       header("Etag: $etag");
@@ -77,7 +77,11 @@ function serveAndCache($fields, $cachefile, $pubdate) {
 
   file_put_contents($cachefile, $jsonout);
   // set lastmodified back to pubdate
-  touch( $cachefile, $pubdate->getTimestamp() );
+  // unless pubdate is in the future :P
+  if ($pubdate->getTimestamp() < $last_modified_time) {
+    $last_modified_time = $pubdate->getTimestamp();
+  }
+  touch( $cachefile, $last_modified_time );
 }
 
 
